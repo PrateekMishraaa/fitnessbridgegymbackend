2d87e188dc886dfe9d8efcd549ce44729e360bc9
prateek mishraaa <pm921670@gmail.com>
2026-06-21 17:11:41 +0530

    fix the cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { bootstrap } from 'global-agent';
 import cloudinary from "./middleware/cloudinary.js";
 bootstrap();
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173','https://fitness-bridge-gym.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 };
